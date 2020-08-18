5476b92395b3146374f3bacb9dd173125d150e83
Ellen Shapiro <designatednerd@gmail.com>
2020-08-17 19:49:40 -0500

    update mock URL transport to be a subclass of RequestChain transport, requiring passing in a store

--- Sources/ApolloTestSupport/MockNetworkTransport.swift
+++ Sources/ApolloTestSupport/MockNetworkTransport.swift
@@ -1,46 +1,20 @@
 @testable import Apollo
 import Dispatch
 
-public final class MockNetworkTransport: NetworkTransport {
-  let body: JSONObject
+public final class MockNetworkTransport: RequestChainNetworkTransport {
 
-  public var clientName = "MockNetworkTransport"
-  public var clientVersion = "mock_version"
-  
-  public init(body: JSONObject) {
-    self.body = body
-  }
-
-  public func send<Operation: GraphQLOperation>(operation: Operation, completionHandler: @escaping (_ result: Result<GraphQLResponse<Operation.Data>, Error>) -> Void) -> Cancellable {
-    DispatchQueue.global(qos: .default).async {
-      completionHandler(.success(GraphQLResponse(operation: operation, body: self.body)))
-    }
-    return MockTask()
-  }
-  
-  public func sendForResult<Operation>(operation: Operation, completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable where Operation : GraphQLOperation {
-    DispatchQueue.global(qos: .default).async {
-      let response = GraphQLResponse(operation: operation, body: self.body)
-      do {
-        let result = try response.parseResultFast()
-        completionHandler(.success(result))
-      } catch {
-        completionHandler(.failure(error))
-      }
-    }
-    
-    return MockTask()
-  }
-}
-
-extension MockNetworkTransport: UploadingNetworkTransport {
-  
-  public func upload<Operation>(operation: Operation, files: [GraphQLFile], completionHandler: @escaping (Result<GraphQLResponse<Operation.Data>, Error>) -> Void) -> Cancellable where Operation : GraphQLOperation {
-    return self.send(operation: operation, completionHandler: completionHandler)
-  }
-  
-  public func uploadForResult<Operation>(operation: Operation, files: [GraphQLFile], completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable where Operation : GraphQLOperation {
-    self.sendForResult(operation: operation, completionHandler: completionHandler)
+  public init(body: JSONObject, store: ApolloStore) {
+    let testURL = TestURL.mockServer.url
+    let mockClient = MockURLSessionClient()
+    mockClient.data = try! JSONSerializationFormat.serialize(value: body)
+    mockClient.response = HTTPURLResponse(url: testURL,
+                                          statusCode: 200,
+                                          httpVersion: nil,
+                                          headerFields: nil)
+    let legacyProvider = LegacyInterceptorProvider(client: mockClient,
+                                                   store: store)
+    super.init(interceptorProvider: legacyProvider,
+               endpointURL: TestURL.mockServer.url)
   }
 }
 
